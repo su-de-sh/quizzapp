6c70b79c16eb80e853425c57942f94a40a21f8e8
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-08-29 22:22:52 +0545

    feat: work started on client side

--- src/App.js
+++ src/App.js
@@ -1,51 +1,64 @@
-import { MCQ } from "./components/MCQ";
-import React, { useState, useEffect } from "react";
-import apiCall from "./services/mcqapi";
-import "./App.css";
+// import { MCQ } from "./components/MCQ";
+// import React, { useState, useEffect } from "react";
+// import apiCall from "./services/mcqapi";
+// import "./App.css";
+
+// const App = () => {
+//   const [questions, setQuestions] = useState([]);
+//   const [questionNumber, setQuestionNumber] = useState(0);
+//   const [score, setScore] = useState(0);
+
+//   useEffect(() => {
+//     apiCall.read().then((data) => setQuestions(data));
+//   }, []);
+
+//   //  checkAnswer
+
+//   const checkAnswer = (event) => {
+//     setQuestionNumber(questionNumber + 1);
+
+//     if (
+//       event.target.innerHTML.trim() === questions[questionNumber].correctAnswer
+//     ) {
+//       setScore(score + 1);
+//     }
+//   };
+
+//   let questionLength = questions.map((q) => q.question).length;
+
+//   return !questionLength ? (
+//     <div>Loading...</div>
+//   ) : questionLength > questionNumber ? (
+//     <div className="body">
+//       <div className="question-count">
+//         <span>Question {questionNumber}</span>/{questionLength}
+//       </div>
+//       <MCQ
+//         questions={questions}
+//         questionNumber={questionNumber}
+//         checkAnswer={checkAnswer}
+//       />
+//     </div>
+//   ) : (
+//     <div className="app">
+//       <div className="score-section">
+//         <div>
+//           Your Score: {score} out of {questionLength}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React from "react";
+import Navbar from "./components/nav/Navbar";
 
 const App = () => {
-  const [questions, setQuestions] = useState([]);
-  const [questionNumber, setQuestionNumber] = useState(0);
-  const [score, setScore] = useState(0);
-
-  useEffect(() => {
-    apiCall.read().then((data) => setQuestions(data));
-  }, []);
-
-  //  checkAnswer
-
-  const checkAnswer = (event) => {
-    setQuestionNumber(questionNumber + 1);
-
-    if (
-      event.target.innerHTML.trim() === questions[questionNumber].correctAnswer
-    ) {
-      setScore(score + 1);
-    }
-  };
-
-  let questionLength = questions.map((q) => q.question).length;
-
-  return !questionLength ? (
-    <div>Loading...</div>
-  ) : questionLength > questionNumber ? (
-    <div className="body">
-      <div className="question-count">
-        <span>Question {questionNumber}</span>/{questionLength}
-      </div>
-      <MCQ
-        questions={questions}
-        questionNumber={questionNumber}
-        checkAnswer={checkAnswer}
-      />
-    </div>
-  ) : (
-    <div className="app">
-      <div className="score-section">
-        <div>
-          Your Score: {score} out of {questionLength}
-        </div>
-      </div>
+  return (
+    <div>
+      <Navbar />
     </div>
   );
 };
