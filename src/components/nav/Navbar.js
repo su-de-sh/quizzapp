9f6769c43675371c00007150b1ab096a6b57a213
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-08-29 23:20:00 +0545

    feat: navbar created

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
 
 const Navbar = () => {
   return (
     <div>
-      <h1>This is navbar component.</h1>
+      <div className="navbar">
+        <h1 className="logo">QUIZZ</h1>
+        <div className="link_side">
+          <Link to="/" className="link">
+            Home
+          </Link>
+          <Link to="/about" className="link">
+            About
+          </Link>
+          <Link to="/contact" className="link">
+            Contact
+          </Link>
+          <Link to="/login" className="link">
+            Login
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
