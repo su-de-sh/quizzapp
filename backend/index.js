3b4db08b7b9a64fb426af43dab93afad78f2aa77
su-de-sh <sudesh7443@gmail.com>
2022-09-01 17:08:03 +0545

    feat: added new favicon

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const Question = require("./models/question");
-const insertAllData = require("./utility/helper_func");
+// const insertAllData = require("./utility/helper_func");
 require("dotenv").config();
 
 app.use(cors());
