c9e3476fa0e7ad9064cc8c2a8ee8eeaa2a9bd9f8
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-09-04 19:56:14 +0545

    feat: styling still on process

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -7,34 +7,36 @@ const Navbar = ({ open, handleOpen, handleClose }) => {
   return (
     <div>
       <div className="navbar">
-        <h1 className="logo">QUIZZ</h1>
-        <div className="link_side">
-          <Link to="/" className="link">
-            Home
-          </Link>
-          <Link to="/quiz" className="link">
-            Quiz
-          </Link>
-          <Link to="/about" className="link">
-            About
-          </Link>
-          <Link to="/contact" className="link">
-            Contact
-          </Link>
-          <Link to="/login" className="link">
-            Login
-          </Link>
-        </div>
-        <div className="hamburger_menu">
-          {!open ? (
-            <>
-              <Menu fontSize="large" onClick={handleOpen}></Menu>
-            </>
-          ) : (
-            <>
-              <Cancel onClick={handleClose} />
-            </>
-          )}
+        <div className="container">
+          <h1 className="logo">QUIZZ</h1>
+          <div className="link_side">
+            <Link to="/" className="link">
+              Home
+            </Link>
+            <Link to="/quiz" className="link">
+              Quiz
+            </Link>
+            <Link to="/about" className="link">
+              About
+            </Link>
+            <Link to="/contact" className="link">
+              Contact
+            </Link>
+            <Link to="/login" className="link">
+              Login
+            </Link>
+          </div>
+          <div className="hamburger_menu">
+            {!open ? (
+              <>
+                <Menu fontSize="large" onClick={handleOpen}></Menu>
+              </>
+            ) : (
+              <>
+                <Cancel onClick={handleClose} />
+              </>
+            )}
+          </div>
         </div>
       </div>
     </div>
